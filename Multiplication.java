60a4de021847730b3e0fec308b20d82509340dde
Pratik Deshmukh <143001689+pratik240298@users.noreply.github.com>
2024-03-14 08:44:46 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+public class Multiplication
+{
+int a=9;
+int b=7;
+public void mul()
+{
+int res= a*b;
+System.out.println(res);
+}
+public static void main(String args[])
+{
+Multiplication m =new Multiplication();
+m.mul();
+}
+}
